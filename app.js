a6324452292401d2e35cf502991a36b1cf014e37
Luis Marques <luisserafim99@gmail.com>
2020-06-10 19:09:07 +0100

    swagger and app.js

--- app.js
+++ app.js
@@ -4,97 +4,104 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
+// const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerDocument = require('./swagger.json');
 
 const testsRouter = require('./api/routes/tests');
 const usersRouter = require('./api/routes/users');
-const cors = require('cors');
 
 const mongoose = require('./api/config/database'); //database configuration
 
 const app = express();
 
+// const swaggerOptions = {
+// 	swaggerDefinition: {
+// 		info: {
+// 			title: 'Covid Tests Manager API',
+// 			description: 'REST API Covid Tests Manager application\n\nGitHub Repository: https://github.com/LuisMarques99/ProjetoPAW2020\n\nAuthors:\nDiogo Costa: https://github.com/diogocosta4\nLuis Marques: https://github.com/LuisMarques99\nLuis Teixeira: https://github.com/luisteixeira92',
+// 			version: '2.0.0',
+// 			contact: {
+// 				name: 'Grupo 39'
+// 			}
+// 		},
+// 		host: 'localhost:3000',
+// 		basePath: '/api/v1',
+// 	},
+// 	apis: [
+// 		'app.js',
+// 		'.api/routes/*.js'
+// 	]
+// };
+// const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-
-
-
-const options = {
-	// line 27
-	swaggerDefinition: {
-		info: {
-			title: 'swagger-express-jsdoc', // Title (required)
-			version: '1.0.1', // Version (required)
-		},
-		host: 'localhost:3000',
-		basePath: '/',
-	},
-	apis: ['./users'], // Path to the API docs
-};
-const swaggerSpec = swaggerJSDoc(options);
 
 function validateUser(req, res, next) {
-	jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (
-		err,
-		decoded
-	) {
-		if (err) {
-			res.json({ status: 'error', message: err.message, data: null });
-		} else {
-			// add user id to request
-			req.body.userId = decoded.id;
-			next();
+	jwt.verify(
+		req.headers['x-access-token'],
+		req.app.get('secretKey'),
+		function (err, decoded) {
+			if (err) {
+				res.json({ status: 'error', message: err.message, data: null });
+			} else {
+				// add user id to request
+				req.body.userId = decoded.id;
+				next();
+			}
 		}
-	});
+	);
 }
 
-app.set('secretKey', 'Y{ca%n75U!_>,@c'); // jwt secret token
+
+app.set('secretKey', 'Y{ca%n75U!_>,@c') // jwt secret token
+
 app.use(cors());
+
 app.use('/public', express.static('public'));
 
-app
-	.use(express.json())
+app.use(logger('dev'))
 
-	.use(logger('dev'))
+app.use(express.json());
 
-	.get('/api-docs.json', function (req, res) {
-		// line 41
-		res.setHeader('Content-Type', 'application/json');
-		res.send(swaggerSpec);
-	})
-	.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get('/api-docs.json', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(swaggerSpec);
+})
 
-	.use(bodyParser.urlencoded({ extended: false }))
-	.use(bodyParser.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-	.get('/', function (req, res) {
-		res.json({ estado: 'ok' });
-	})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-	// public route
-	.use('/api/v1/users', usersRouter)
+app.use(bodyParser.json());
 
-	// private route
-	.use('/api/v1/tests', validateUser, testsRouter)
+app.get('/', function (req, res) {
+	res.json({ estado: 'ok' });
+});
 
-	.get('/favicon.ico', function (req, res) {
-		res.sendStatus(204);
-	})
+// public route
+app.use('/api/v1/users', usersRouter);
 
-	// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
-	// handle 404 error
-	.use(function (req, res, next) {
-		let err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	})
+// private route
+app.use('/api/v1/tests', validateUser, testsRouter);
 
-	// handle errors
-	.use(function (err, req, res, next) {
-		console.log(err);
+app.get('/favicon.ico', function (req, res) {
+	res.sendStatus(204);
+});
 
-		if (err.status === 404) res.status(404).json({ message: 'Not found' });
-		else res.status(500).json({ message: 'Something looks wrong :( !!!' });
-	});
+// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
+// handle 404 error
+app.use(function (req, res, next) {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// handle errors
+app.use(function (err, req, res, next) {
+	console.log(err);
+
+	if (err.status === 404) res.status(404).json({ message: 'Not found' });
+	else res.status(500).json({ message: 'Something looks wrong :( !!!' });
+});
 
 module.exports = app;
